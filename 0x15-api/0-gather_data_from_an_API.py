d2eba56887a1aca6d9e4d8dc5924ebd7cc04f467
dcode3244 <dagimourgie111@gmail.com>
2023-02-08 09:30:55 +0000

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' for a given employee id returns information about
+employee todo list progress
+'''
+
+from sys import argv
+import requests
+
+nameUrl = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+todoUrl = 'https://jsonplaceholder.typicode.com/todos'
+
+name = requests.get(nameUrl).json().get('name')
+todos = requests.get(todoUrl, params={'userId': int(argv[1])}).json()
+
+total = len(todos)
+done = 0
+
+for todo in todos:
+    if todo.get('completed') is True:
+        done += 1
+
+print('Employee {} is done with tasks({}/{}):'.format(name, done, total))
+for todo in todos:
+    if todo.get('completed') is True:
+        print('\t', todo.get("title"))
