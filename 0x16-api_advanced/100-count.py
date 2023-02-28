86a5ba46b0f0ed719ec5a1352257d45b092769f0
dcode3244 <dagimourgie111@gmail.com>
2023-02-28 20:27:09 +0300

    bug fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns a list containing the titles
-of all hot articles for a given subreddit.
+queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords
+(case-insensitive, delimited by spaces.).
 """
 import requests
 
 
 def count_words(subreddit, word_list, hot_list=[], key_words={}):
     """
-        returns the list containing titles of all hot articles
-        for a given subreddit.
+        prints sorted count of given keywords from a given subreddit
+        hot titles.
     """
     if len(hot_list):
         after = hot_list[0]
@@ -27,7 +28,8 @@ def count_words(subreddit, word_list, hot_list=[], key_words={}):
     for topic in topics:
         title = topic.get('data').get('title').lower()
         for word in word_list:
-            num = title.lower().count(word.lower())
+            word = word.lower()
+            num = title.count(word)
             if key_words.get(word) is None:
                 if num != 0:
                     key_words[word] = num
@@ -37,14 +39,18 @@ def count_words(subreddit, word_list, hot_list=[], key_words={}):
     after = res.json().get('data').get('after')
     h_list = [("after={}".format(after))]
     if after is None:
-        count = [num for num in key_words.values()]
-        count.sort(reverse=True)
-        keys = (list(key_words.keys()))
-        keys.sort()
-        sorted_key_words = {i: key_words[i] for i in keys}
-        for n in count:
-            for k, v in sorted_key_words.items():
-                if v == n:
-                    print("{}: {}".format(k, v))
+        if len(key_words) > 0:
+            sorted_print(key_words)
         return
     count_words(subreddit, word_list, h_list, key_words)
+
+
+def sorted_print(key_words):
+    """ prints words with their count in descending order """
+    s_key = list(key_words.keys())
+    s_key.sort()
+    sorted_by_key = {i: key_words[i] for i in s_key}
+    s_val = sorted(sorted_by_key.items(), key=lambda x: x[1], reverse=True)
+    s_val = dict(s_val)
+    for key, value in s_val.items():
+        print("{}: {}".format(key, value))
