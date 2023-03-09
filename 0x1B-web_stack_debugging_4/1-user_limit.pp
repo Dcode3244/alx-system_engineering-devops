9b0a1083c9af948b87fa0737165083abdbe94511
dcode3244 <dagimourgie111@gmail.com>
2023-03-09 14:19:46 +0300

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Allow the user holberton to login without error.
+
+exec { 'increase_hard_file_limit':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase_soft_file_limit':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
